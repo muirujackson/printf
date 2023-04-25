2a2ea2359c9c9513fb7e7b3e5afda5a617eea625
muirujackson <jakezweru@gmail.com>
2023-04-25 08:29:43 -0700

    check edge cases

--- print.c
+++ print.c
@@ -23,22 +23,33 @@ int _printf(const char *format, ...)
 	i = 0;
 	if (format == NULL)
 		return (-1);
+
 	while (format && format[i])
 	{
 		j = 0;
 
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			i++;
-			if (!format[i])
-				return (-1);
-			while (j < 5 && (format[i] != *(funcs[j].fmt)))
-				j++;
-			if (j < 5)
-			{
-				funcs[j].fn(ap);
-			}
-			i++;
+			_putchar(format[i]);
+			continue;
+		}
+
+		if (format[i + 1] == '\0')
+			return (-1);
+
+		if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			i += 2;
+			continue;
+		}
+		while (j < 5 && (format[i] != *(funcs[j].fmt)))
+			j++;
+		if (j < 5)
+		{
+			funcs[j].fn(ap);
+			i += 2;
+			continue;
 		}
 		_putchar(format[i]);
 		i++;
