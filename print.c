bcf7f5385a21a09fe35981c58ff0ecfee67db49d
muirujackson <jakezweru@gmail.com>
2023-04-25 19:18:46 -0700

    removed unnecessary files

--- print.c
+++ print.c
@@ -3,8 +3,6 @@
 /**
  * print_helper - check the format
  * @format: the format to be printed
- * @arg: all arguments
- * @i: index
  *
  * Return: number of int printed
  */
@@ -17,16 +15,14 @@ int print_helper(const char *format, va_list arg, int i)
 		{'d', print_int},
 		{'i', print_int},
 		{'b', int_to_bin}
-	};
-	while (j < 5 && (format[i + 1] != (funcs[j].fmt)))
+		};
+	while (j < 5 && (format[i+1] != (funcs[j].fmt)))
 		j++;
 	if (j < 5)
 	{
 		k = funcs[j].fn(arg);
-		if (k < 0)
-			return (-1);
 		return (k);
-	}
+    }
 	return (k);
 }
 
@@ -46,37 +42,36 @@ int _printf(const char *format, ...)
 	va_start(ap, format);
 	if (format == NULL)
 		return (-1);
-	if (!format[i])
-		return (0);
+
 	while (format && format[i] != '\0')
 	{
 		if (format[i] != '%')
 		{
-			write(1, &format[i], 1);
+		    write(1, &format[i], 1);
 			i++;
-		} else 
-		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			else if (format[i + 1] == '%')
-			{
-				j = j - 1;
-				write(1, "%", 1);
-				i += 2;
-				continue;
-			}
-			k = print_helper(format, ap, i);
-			if (k < 0)
-				return -1;
-			else
-			{
-				i += 2;
-				k -= 2;
-				continue;
-			}
-			write(1, &format[i], 1);
-			i++;
+			continue;
 		}
+
+		if (format[i + 1] == '\0')
+			return (-1);
+
+		if (format[i + 1] == '%')
+		{
+			j = j - 1;
+			write(1, "%", 1);
+			i += 2;
+			continue;
+		}
+		k = print_helper(format, ap, i);
+
+		if (k > 0)
+		{
+			i += 2;
+			k -= 2;
+			continue;
+		}
+		write(1, &format[i], 1);
+		i++;
 	}
 	va_end(ap);
 	return (i + k + j);
