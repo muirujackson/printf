7cfc16aec747b7d75a74e0475898d413d8b7e421
muirujackson <jakezweru@gmail.com>
2023-04-28 20:12:09 -0700

    gg

--- main.c
+++ main.c
@@ -14,12 +14,12 @@ int main(void)
     unsigned int ui;
     void *addr;
 
-    len = _printf("%s", "This sentence is retrieved from va_args!\n");
-    len2 = _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
+    len = _printf("Length:[%b, %i]\n", len, len);
+    len2 = printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
@@ -34,10 +34,11 @@ int main(void)
     printf("String:[%s]\n", "I am a string !");
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
+    _printf("Percent:[%%]\n");
+    printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
     return (0);
 }
